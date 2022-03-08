96a2ca08a9b17ff19a5e6fca1a9003dcb5448743
MiltonLai <millton.lai@gmail.com>
2022-03-08 10:23:02 +0800

    opt: add test order

--- pgdemo-impl/src/test/java/com/rockbb/pgdemo/commons/test/AccountTest.java
+++ pgdemo-impl/src/test/java/com/rockbb/pgdemo/commons/test/AccountTest.java
@@ -3,7 +3,10 @@ package com.rockbb.pgdemo.commons.test;
 import com.rockbb.pgdemo.commons.api.dto.AccountDTO;
 import com.rockbb.pgdemo.commons.impl.mapper.AccountMapper;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -17,11 +20,13 @@ import java.util.List;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class AccountTest {
     @Resource
     AccountMapper accountMapper;
 
     @Test
+    @Order(1)
     void list() {
         System.out.println("hello");
         List<AccountDTO> accounts = accountMapper.list(new HashMap<>());
@@ -29,6 +34,7 @@ public class AccountTest {
     }
 
     @Test
+    @Order(2)
     void insert() {
         AccountDTO account = new AccountDTO();
         account.setName("test");
